54b8fa37cf02203c619bc0089053bcc632fd911c
atlogan <drewthomaslogan5201@gmail.com>
2025-05-19 10:34:05 -0400

    implemented permissions for update/delete

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Student # Import your specific model
 
 # Create your views here.
@@ -17,21 +17,21 @@ class StudentCreateView(LoginRequiredMixin, CreateView):
     template_name = 'myapp/student_form.html' # Optional: Defaults to myapp/student_form.html
     fields = ['name', 'major', 'enrollment_date'] # Specify fields to include in the form OR use form_class
     success_url = reverse_lazy('student_list') # Redirect after successful creation
-    # Alternatively define get_success_url(self) method
-    def form_valid(self, form):
-        # Assuming your model has an 'owner' ForeignKey to User
-        # Make sure 'owner' is NOT in the 'fields' attribute of the view
-        form.instance.owner = self.request.user
-        return super().form_valid(form)
 
 
-class StudentUpdateView(LoginRequiredMixin, UpdateView):
+class StudentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Student
     template_name = 'myapp/student_form.html' # Can reuse the create form template
     fields = ['name', 'major', 'enrollment_date'] # Specify fields
     success_url = reverse_lazy('student_list') # Redirect after successful update
-
-class StudentDeleteView(LoginRequiredMixin, DeleteView):
+    
+    def test_func(self):
+        return self.request.user.is_staff
+    
+class StudentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Student
     template_name = 'myapp/student_confirm_delete.html' # Optional: Defaults to myapp/student_confirm_delete.html
     success_url = reverse_lazy('student_list') # Redirect after successful deletion
+
+    def test_func(self):
+        return self.request.user.is_staff
