173f907dccc1bf64fcb694d2877a43df7120587f
atlogan <drewthomaslogan5201@gmail.com>
2025-05-19 14:58:56 -0400

    configured url routing

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from myapp import views
+
+router = DefaultRouter()
+router.register(r'students', views.StudentViewSet, basename='student')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('myapp.urls')),
+    path('api/', include(router.urls))
 ]
