e29eb09d8fa0134a0fc3fd48d5ec9423773e5e35
atlogan <drewthomaslogan5201@gmail.com>
2025-05-27 09:39:12 -0400

    add django.contrib.sites to installed_apps

--- myproject/settings.py
+++ myproject/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'rest_framework',
     'myapp',
     'allauth',
